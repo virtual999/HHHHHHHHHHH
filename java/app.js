b1a2de9d72045104cdc4cf83762199eee6fad4e9
artemonishchenko <144786041+artemonishchenko@users.noreply.github.com>
2023-09-25 01:01:36 +0300

    Add files via upload

--- java/app.js
+++ java/app.js
@@ -2,3 +2,8 @@ window.addEventListener('scroll', e =>{
   document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
 })
 
+gsap.registerPlagin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+  wrapper: '.wrapper',
+  content: '.content'
+})
