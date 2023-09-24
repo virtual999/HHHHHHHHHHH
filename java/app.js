a96515d30e79f0e39adde6af1ecc67142bb140fc
artemonishchenko <144786041+artemonishchenko@users.noreply.github.com>
2023-09-25 01:02:10 +0300

    Update app.js

--- java/app.js
+++ java/app.js
@@ -1,9 +1,3 @@
 window.addEventListener('scroll', e =>{
   document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
 })
-
-gsap.registerPlagin(ScrollTrigger, ScrollSmoother)
-ScrollSmoother.create({
-  wrapper: '.wrapper',
-  content: '.content'
-})
